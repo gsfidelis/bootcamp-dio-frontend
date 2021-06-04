24d67eafd7ba07c48042f0cdb041fef029a488b2
gsfidelis <gfidelis@live.com>
2021-06-03 21:44:18 -0300

    Primeira versão do rontend - DIO.

---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,13 @@
+class Stock {
+
+  id: number;
+  name: string;
+  price: number;
+  minimum: number | undefined;
+  exclusiveMinimum: boolean;
+  variation: number;
+  date: string;
+
+}
+
+export default Stock;
